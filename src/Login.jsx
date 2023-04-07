4f70018374164da838221bf9a5bb3f9ee52acf1a
Jefferson Ventura <jeffersonsilvamuz@gmail.com>
2023-04-07 04:30:26 -0300

    feat: login page call api

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,23 +1,42 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
+import axios from "axios";
+import React, { useState } from "react"
+import { Link, useNavigate } from 'react-router-dom';
 
 function Login() {
+    const [values, setValues] = useState({
+        email: '',
+        password: ''
+    })
+    const navigate = useNavigate()
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        axios.post('http://localhost:8081/login', values)
+        .then(res => {
+            if(res.data.Status === "Sucess"){
+                navigate('/');
+            }else {
+                alert("Error");
+            }
+        })
+        .catch(err => console.log(err));
+    }
+
     return ( 
         <div className="d-flex justify-content-center align-items-center bg-primary vh-100">
         <div className="bg-white p-3 rounded w-25">
             <h2>Login</h2>
-            <form>
+            <form onSubmit={handleSubmit}>
             <div className="mb-3">
                 <label htmlFor="email">
                 <strong>Email</strong>
                 </label>
-                <input type="email" placeholder="Enter Email" name="email" className="form-control rounded-0" />
+                <input type="email" placeholder="Enter Email" name="email" onChange={e => setValues({...values, email: e.target.value})} className="form-control rounded-0" />
             </div>
             <div className="mb-3">
                 <label htmlFor="password">
                 <strong>Password</strong>
                 </label>
-                <input type="password" placeholder="Enter Password" name="password" className="form-control rounded-0" />
+                <input type="password" placeholder="Enter Password" name="password" onChange={e => setValues({...values, password: e.target.value})} className="form-control rounded-0" />
             </div>
             <button type="submit" className="btn btn-success w-100 rounded-0">Login</button>
             <Link to="/register" className="btn btn-default border w-100 bg-light rounded-0 text-decoration-none">Create Account</Link>
